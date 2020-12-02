c0fc28d44edf4797b13e48f7a95c3332245f8ee5
Elvis <68830584+ElvisSosa27@users.noreply.github.com>
2020-12-02 05:22:05 -0400

    Add files via upload

---
+++ ProyectoFinal_CleanArchitecture/CleanArchitecture.api/Controllers/ControladorPublic.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Infrastructure.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControladorPublic : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            var publicaciones = new Repositorio_Publicar().GetPosts();
+            return Ok(publicaciones);
+        }
+    }
+}
